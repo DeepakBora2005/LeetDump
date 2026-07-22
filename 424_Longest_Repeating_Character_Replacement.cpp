36b1c1802d343deae7bf4d074a69244ec9702c56
Deepak <boradeepakreddy2005@gmail.com>
2026-07-22 11:43:32 +0530

    Sync: 424. Longest Repeating Character Replacement (Medium) - Leet2Git

---
+++ 424_Longest_Repeating_Character_Replacement.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        vector<int> hash(n, 0);
+
+        int maxLen = 0;
+
+        for(int i = 0; i < n; i++) {
+            int maxFreq = 0;
+
+            for(int j = i; j < n; j++) {
+                hash[j]++;
+                maxFreq = max(maxFreq, hash[j]);
+                int changes = (j - i + 1) - maxFreq;
+
+                if(changes <= k) {
+                    maxLen = max(maxLen, (j - i + 1));
+                }
+                else {
+                    break;
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
