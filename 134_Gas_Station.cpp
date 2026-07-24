4f47465328044f5583dbf34de22c61afe76641d7
Deepak <boradeepakreddy2005@gmail.com>
2026-07-24 21:01:24 +0530

    Sync: 134. Gas Station (Medium) - Leet2Git

--- 134_Gas_Station.cpp
+++ 134_Gas_Station.cpp
@@ -2,6 +2,16 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
+        
+        int totalGas = 0;
+        int totalCost = 0;
+
+        for(int i = 0; i < n; i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+
+        if(totalGas < totalCost) return -1;
 
         for(int i = 0; i < n; i++) {
             int curr = i;
