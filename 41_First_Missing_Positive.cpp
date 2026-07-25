db37bed07500e8dae18eee5a32587930f05ec01a
Deepak <boradeepakreddy2005@gmail.com>
2026-07-25 13:10:24 +0530

    Sync: 41. First Missing Positive (Hard) - Leet2Git

--- 41_First_Missing_Positive.cpp
+++ 41_First_Missing_Positive.cpp
@@ -2,25 +2,19 @@ class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        int totalSum = n * (n + 1) / 2;
-        int sum = 0;
-        int arr[n + 1];
-        int ans;
-
+        int ans = 0;
+        
         for(int i = 0; i < n; i++) {
-            if(nums[i] < 1 || nums[i] > n) {
-                nums[i] = 0;
-            }
-            else {
-                arr[nums[i]] = 1;
-                sum += nums[i];
+            while(nums[i] >= 1 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
 
-        for(int i = 1; i <= n; i++) {
-            if(arr[i] == 0) {
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != i + 1) {
                 ans = i;
-            } 
+                break;
+            }
         }
 
         return ans;
