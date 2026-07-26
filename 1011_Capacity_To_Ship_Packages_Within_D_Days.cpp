1d95e4abe4daeb2ecab02f578921434f6addf36c
Deepak <boradeepakreddy2005@gmail.com>
2026-07-26 13:10:34 +0530

    Sync: 1011. Capacity To Ship Packages Within D Days (Medium) - Leet2Git

---
+++ 1011_Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool possible(int mid, vector<int> &piles, int h) {
+        long long sum = 0;
+
+        for(int pile : piles) {
+            sum += ceil((double)pile / mid);
+
+            if(sum > h) return false;
+        }
+
+        return true;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = 1;
+        int n = piles.size();
+        int ans = high;
+        
+        for(int i = 0; i < n; i++) {
+            high = max(high, piles[i]);
+        }
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(possible(mid, piles, h)) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else  {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
