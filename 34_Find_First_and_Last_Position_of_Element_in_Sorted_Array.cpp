c6a564771b677112b01476cda093efffe52646a4
Deepak <boradeepakreddy2005@gmail.com>
2026-07-27 00:28:54 +0530

    Sync: 34. Find First and Last Position of Element in Sorted Array (Medium) - Leet2Git

--- 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.cpp
@@ -43,7 +43,7 @@ public:
             }
         }
         
-        if(high >= 0 && nums[high + 1] == target) {
+        if(high >= 0 && nums[low] == target) {
             last = high;
         }
         else {
