66c745649955516cdba2a932f63e9afadf8c42cb
Deepak <boradeepakreddy2005@gmail.com>
2026-07-27 23:29:40 +0530

    Sync: 162. Find Peak Element (Medium) - Leet2Git

--- 162_Find_Peak_Element.cpp
+++ 162_Find_Peak_Element.cpp
@@ -3,44 +3,20 @@ public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
 
-        if(n == 1) return 0;
+        int low = 0;
+        int high = n - 1;
 
-        if(n == 2) {
-            if(nums[0] > nums[1]) return 0;
-            else return 1;
-        }
-
-        int index;
-        int ans;
-
-        if(nums[0] <= nums[n - 1]) {
-            ans = nums[n - 1];
-            index = n - 1;
-        }
-        else {
-            ans = nums[0];
-            index = 0;
-        }
-
-        int low = 1;
-        int high = n - 2;
-
-        while(low <= high) {
+        while(low < high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] > ans) {
-                index = mid;
-                ans = nums[mid];
-            }
-
-            if(nums[mid + 1] > nums[mid]) {
-                low = mid + 1;
+            if(nums[mid] > nums[mid + 1]) {
+                high = mid;
             }
             else {
-                high = mid - 1;
+                low = mid + 1;
             }
         }
 
-        return index;
+        return low;
     }
 };
