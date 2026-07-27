3973269de011ec73c5f67cb470b2976e021aba36
Deepak <boradeepakreddy2005@gmail.com>
2026-07-27 18:58:18 +0530

    Sync: 1539. Kth Missing Positive Number (Easy) - Leet2Git

---
+++ 1539_Kth_Missing_Positive_Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        unordered_map<int, int> mp;
+
+        int n = arr.size();
+
+        for(int i = 0; i < n; i++) {
+            mp[arr[i]]++;
+        }
+
+        int ans = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(mp[i] == 0) k--;
+
+            if(k == 0) ans = i;
+        }
+
+        return ans;
+    }
+};
