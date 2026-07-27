69da0801bb86414f7f6aa402a4d6a4f35a9482a3
Deepak <boradeepakreddy2005@gmail.com>
2026-07-27 21:43:34 +0530

    Sync: 153. Find Minimum in Rotated Sorted Array (Medium) - Leet2Git

--- 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -3,43 +3,27 @@ public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
 
-        if(n == 1) return nums[0];
+        int low = 0;
+        int high = n - 1;
 
-        if(n == 2) {
-            return min(nums[0], nums[1]);
-        }
-
-        int mini = min(nums[0], nums[n - 1]);
-
-        int low = 1;
-
-        int high = n - 2;
+        int ans = INT_MAX;
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] < mini) {
-                mini = nums[mid];
+            if(nums[mid] < ans) {
+                ans = nums[mid];
             }
 
-            if(nums[mid] > nums[low]) {
-                if(nums[low] < mini) {
-                    high = mid - 1;
-                }
-                else {
-                    low = mid + 1;
-                }
+            if(nums[low] < nums[mid]) {
+                ans = nums[low];
+                low = mid + 1;
             }
             else {
-                if(nums[high] < mini) {
-                    low = mid + 1;
-                }
-                else {
-                    high = mid - 1;
-                }
+                high = mid - 1;
             }
         }
 
-        return mini;
+        return ans;
     }
 };
