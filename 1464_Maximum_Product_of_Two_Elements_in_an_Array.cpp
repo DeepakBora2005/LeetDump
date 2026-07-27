e2b7ecc70864366d2e43ec7eb457847984c006ea
Deepak <boradeepakreddy2005@gmail.com>
2026-07-27 09:50:12 +0530

    Sync: 1464. Maximum Product of Two Elements in an Array (Easy) - Leet2Git

--- 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -3,8 +3,19 @@ public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
 
-        sort(nums.begin(), nums.end());
+        int first = nums[0];
+        int second = nums[0];
 
-        return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+        for(int i = 1; i < n; i++) {
+            if(nums[i] >= first) {
+                second = first;
+                first = nums[i];
+            }
+            if(nums[i] < first && nums[i] > second) {
+                second = nums[i];
+            }
+        }
+
+        return (first - 1) * (second - 1);
     }
 };
