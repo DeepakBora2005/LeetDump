8cd0c3c1189a29c22534742725898f691b57c913
Deepak <boradeepakreddy2005@gmail.com>
2026-07-28 21:02:28 +0530

    Sync: 4. Median of Two Sorted Arrays (Hard) - Leet2Git

---
+++ 4_Median_of_Two_Sorted_Arrays.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+
+        if(m + n == 0) return 0.0;  
+
+        int left = 0;
+        int right = 0;
+        int k = 0;
+
+        vector<int> temp(m + n);
+
+        while(left < m && right < n) {
+            if(nums1[left] <= nums2[right]) {
+                temp[k++] = nums1[left++];
+            }
+            else {
+                temp[k++] = nums2[right++];
+            }
+        }
+
+        while(left < m) {
+            temp[k++] = nums1[left++];
+        }
+
+        while(right < n) {
+            temp[k++] = nums2[right++];
+        }
+
+        double ans = 0;
+
+        if((m + n) % 2 == 0) {
+            ans = temp[(m + n) / 2] + temp[((m + n) / 2) - 1];
+        }
+        else {
+            ans = temp[(m + n) / 2];
+        }
+
+        return ans;
+    }
+};
