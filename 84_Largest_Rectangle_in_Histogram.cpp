c6e742ccdd0dc5132e4dd9cef860546c8873de1e
Deepak <boradeepakreddy2005@gmail.com>
2026-07-29 18:16:50 +0530

    Sync: 84. Largest Rectangle in Histogram (Hard) - Leet2Git

---
+++ 84_Largest_Rectangle_in_Histogram.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& h) {
+        int n = h.size();
+
+        stack<int> st;
+        vector<int> prefix(n, 0);
+        vector<int> suffix(n, 0);
+
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && h[st.top()] >= h[i]) {
+                st.pop();
+            }
+
+            st.empty() ? prefix[i] = 0 : prefix[i] = st.top();
+
+            st.push(i);
+        }
+
+        while(!st.empty()) {
+            st.pop();
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && h[st.top()] >= h[i]) {
+                st.pop();
+            }
+
+            st.empty() ? suffix[i] = 0 : prefix[i] = st.top();
+
+            st.push(i);
+        }
+
+        int maxArea = 0;
+
+        for(int i = 0; i < n; i++) {
+            int width = suffix[i] - prefix[i];
+            int area = h[i] * width;
+
+            maxArea = max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+};
