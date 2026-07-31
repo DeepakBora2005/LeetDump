419ccec7489b3fa7ae55cf26f1cdc0ebde8b1f62
Deepak <boradeepakreddy2005@gmail.com>
2026-07-31 21:07:31 +0530

    Sync: 135. Candy (Hard) - Leet2Git

--- 135_Candy.cpp
+++ 135_Candy.cpp
@@ -2,20 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int ans = 1;
+        int ans = 0;
 
-        vector<int> store(n, 0);
-        store[0] = 1;
+        vector<int> store(n, 1);
 
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i - 1]) {
-                ans += store[i - 1] + 1;
                 store[i] = store[i - 1] + 1;
-            }
-            else {
-                ans++;
-                store[i] = 1;
-            }
+            } 
+        }
+
+        for(int i = n - 2; i >= 0; i--) {
+            store[i] = max(store[i], store[i + 1] + 1);
+        }
+
+        for(int num : ratings) {
+            ans += num;
         }
 
         return ans;
