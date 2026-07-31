460cda105a526c049e48a971f71897686715d9ec
Deepak <boradeepakreddy2005@gmail.com>
2026-07-31 21:00:24 +0530

    Sync: 135. Candy (Hard) - Leet2Git

--- 135_Candy.cpp
+++ 135_Candy.cpp
@@ -5,22 +5,18 @@ public:
 
         int n = ratings.size();
         int ans = 1;
-        int prev = 1;
+
+        vector<int> store(n, 0);
+        store[0] = 1;
 
         for(int i = 1; i < n; i++) {
             if(ratings[i] > ratings[i - 1]) {
-                if(ratings[i - 1] == 0) {
-                    int temp = 1;
-                    ans += (temp + 1);
-                    prev = temp;
-                }
-                else {
-                    ans += (ratings[i - 1] + 1);
-                    prev = ratings[i - 1];
-                }
+                ans += store[i - 1] + 1;
+                store[i] = store[i - 1] + 1;
             }
             else {
-                ans += prev;
+                ans++;
+                store[i] = 1;
             }
         }
 
