f02b05c5066d1500ec8d062a312002b08aa0b1ac
Deepak <boradeepakreddy2005@gmail.com>
2026-08-01 13:11:33 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -1,28 +1,22 @@
 class Solution {
 private: 
-    int dp[20][20][2];
-
     int find(vector<int> &nums, int start, int end, int turn) {
         if(start == end) {
             return (turn == 0) ? nums[start] : 0;
-        }
-            
-        if(dp[start][end][turn] != -1) {
-            return dp[start][end][turn];
         }
 
         if(turn == 0) {
             int leftMost = nums[start] + find(nums, start + 1, end, 1);
             int rightMost = nums[end] + find(nums, start, end - 1, 1);
 
-            return dp[start][end][turn] = max(leftMost, rightMost);
+            return max(leftMost, rightMost);
         }
         else 
         {
             int leftMost = find(nums, start + 1, end, 0);
             int rightMost = find(nums, start, end - 1, 0);
 
-            return dp[start][end][turn] = min(leftMost, rightMost);
+            return min(leftMost, rightMost);
         }
     }
 
@@ -33,9 +27,7 @@ public:
         for(int num : nums) {
             totalSum += num;
         }
-
-        memset(dp, -1, sizeof(dp));
-
+        
         int player1 = find(nums, 0, n - 1, 0);
         int player2 = totalSum - player1;
 
