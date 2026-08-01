a4c5f34a70c9de042968cc247bb2ce78f0cc976c
Deepak <boradeepakreddy2005@gmail.com>
2026-08-01 12:51:45 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int find(vector<int> &nums, int start, int end) {
-        if(start == end) return nums[start];
+    int find(vector<int> &nums, int start, int end, vector<vector<int>> &dp) {
+        if(start == end) {
+            return nums[start];
+        }
+            
+        if(dp[start][end] != -1) {
+            return dp[start][end];
+        }
 
-        int leftMost = nums[start] - find(nums, start + 1, end);
+        int leftMost = nums[start] - find(nums, start + 1, end, dp);
 
-        int rightMost = nums[end] - find(nums, start, end - 1);
-
-        return max(leftMost, rightMost);
+        int rightMost = nums[end] - find(nums, start, end - 1, dp);
+        
+        return dp[start][end] = max(leftMost, rightMost);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
 
-        return find(nums, 0, n - 1) >= 0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return find(nums, 0, n - 1, dp) >= 0;
     }
 };
