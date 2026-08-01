12be9bb84feaa7780a78ff6a6eb37ba25b44327a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-01 18:45:27 +0530

    Sync: 877. Stone Game (Medium) - Leet2Git

--- 877_Stone_Game.cpp
+++ 877_Stone_Game.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
-    int find(vector<int> &piles, int start, int end, vector<vector<int>> &dp) {
-        if(start == end) {
-            return piles[start];
-        }
-
-        if(dp[start][end] != -1) {
-            return dp[start][end];
-        }
-
-        int leftMost = piles[start] - find(piles, start + 1, end, dp);
-        int rightMost = piles[end] - find(piles, start, end - 1, dp);
-
-        return dp[start][end] = max(leftMost, rightMost);
-    }
-
     bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1));
-
-        return find(piles, 0, n - 1, dp) > 0;
+        return true;
     }
 };
