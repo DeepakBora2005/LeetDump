d31510701b686b3d5287849e1897a8d14a23f52a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-01 13:10:26 +0530

    Sync: 486. Predict the Winner (Medium) - Leet2Git

--- 486_Predict_the_Winner.cpp
+++ 486_Predict_the_Winner.cpp
@@ -1,26 +1,44 @@
 class Solution {
-public:
-    int find(vector<int> &nums, int start, int end, vector<vector<int>> &dp) {
+private: 
+    int dp[20][20][2];
+
+    int find(vector<int> &nums, int start, int end, int turn) {
         if(start == end) {
-            return nums[start];
+            return (turn == 0) ? nums[start] : 0;
         }
             
-        if(dp[start][end] != -1) {
-            return dp[start][end];
+        if(dp[start][end][turn] != -1) {
+            return dp[start][end][turn];
         }
 
-        int leftMost = nums[start] - find(nums, start + 1, end, dp);
+        if(turn == 0) {
+            int leftMost = nums[start] + find(nums, start + 1, end, 1);
+            int rightMost = nums[end] + find(nums, start, end - 1, 1);
 
-        int rightMost = nums[end] - find(nums, start, end - 1, dp);
-        
-        return dp[start][end] = max(leftMost, rightMost);
+            return dp[start][end][turn] = max(leftMost, rightMost);
+        }
+        else 
+        {
+            int leftMost = find(nums, start + 1, end, 0);
+            int rightMost = find(nums, start, end - 1, 0);
+
+            return dp[start][end][turn] = min(leftMost, rightMost);
+        }
     }
 
+public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        int totalSum = 0;
+        for(int num : nums) {
+            totalSum += num;
+        }
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        memset(dp, -1, sizeof(dp));
 
-        return find(nums, 0, n - 1, dp) >= 0;
+        int player1 = find(nums, 0, n - 1, 0);
+        int player2 = totalSum - player1;
+
+        return player1 >= player2;
     }
 };
