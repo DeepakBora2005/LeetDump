2d618f2964fc301b9fd2ebb69d46a1289781a058
Deepak <boradeepakreddy2005@gmail.com>
2026-08-02 13:28:59 +0530

    Sync: 13. Roman to Integer (Easy) - Leet2Git

---
+++ 13_Roman_to_Integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> mp;
+        int n = s.size();
+
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;   
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int ans = 0;
+
+        for(int i = n - 1; i >= 0; i--) {
+            ans += mp[i];
+        }
+
+        return ans;
+    }
+};
