f2e0ec5c5d04d083e29e63269188389edb24691d
Deepak <boradeepakreddy2005@gmail.com>
2026-08-02 11:01:26 +0530

    Sync: 907. Sum of Subarray Minimums (Medium) - Leet2Git

---
+++ 907_Sum_of_Subarray_Minimums.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        stack<int> st;
+
+        int n = arr.size();
+
+        vector<int> nse(n, 1);
+        vector<int> pse(n, 1);
+
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            int index = (st.empty()) ? -1 : st.top();
+
+            pse[i] = i - index;
+
+            st.push(i);
+        }
+
+        while(!st.empty()) {
+            st.pop();
+        }
+
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+
+            int index = (st.empty()) ? n : st.top();
+
+            pse[i] = index - i;
+        }
+
+        int mod = (int)1e9 + 7;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            sum += (pse[i] * nse[i] * arr[i]) % mod;
+        }
+
+        return sum;
+    }
+};
