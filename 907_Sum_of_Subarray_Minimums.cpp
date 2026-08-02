bb29e888ed3951910b50eb71e958b40e22ecbec4
Deepak <boradeepakreddy2005@gmail.com>
2026-08-02 11:25:14 +0530

    Sync: 907. Sum of Subarray Minimums (Medium) - Leet2Git

--- 907_Sum_of_Subarray_Minimums.cpp
+++ 907_Sum_of_Subarray_Minimums.cpp
@@ -2,48 +2,29 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         stack<int> st;
+        int mod = 1e9 + 7;
 
         int n = arr.size();
 
-        vector<int> nse(n, 1);
-        vector<int> pse(n, 1);
+        long long ans = 0;
 
-        for(int i = 0; i < n; i++) {
-            while(!st.empty() && arr[st.top()] > arr[i]) {
+        for(int i = 0; i <= n; i++) {
+            while(!st.empty() && (i == n || arr[st.top()] >= arr[i])) {
+                int index = st.top();
                 st.pop();
+                
+                int left = (st.empty()) ? -1 : st.top();
+                int right = i;
+
+                long long leftCount = index - left;
+                long long rightCount = right - index;
+
+                ans = (ans + (long long)leftCount * rightCount * arr[index]) % mod;
             }
-
-            int index = (st.empty()) ? -1 : st.top();
-
-            pse[i] = i - index;
 
             st.push(i);
         }
 
-        while(!st.empty()) {
-            st.pop();
-        }
-
-        for(int i = n - 1; i >= 0; i--) {
-            while(!st.empty() && arr[st.top()] >= arr[i]) {
-                st.pop();
-            }
-
-            int index = (st.empty()) ? n : st.top();
-
-            nse[i] = index - i;
-
-            st.push(i);
-        }
-
-        long long mod = 1e9 + 7;
-        long long sum = 0;
-
-        for(int i = 0; i < n; i++) {
-            long long temp = ((long long)pse[i] * nse[i] * arr[i]) % mod;
-            sum = (sum + temp) % mod;
-        }
-
-        return sum;
+        return ans;
     }
 };
