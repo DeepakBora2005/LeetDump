8ed97d9cb885f46246c9b0e7bb4b2b8c56b910fa
Deepak <boradeepakreddy2005@gmail.com>
2026-08-03 21:45:30 +0530

    Sync: 1406. Stone Game III (Hard) - Leet2Git

---
+++ 1406_Stone_Game_III.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int find(int start, int n, vector<int> &nums, vector<int> &dp) {
+        if(start >= n) return 0;
+
+        if(dp[start] != -1) return dp[start];
+
+        int first = nums[start] - find(start + 1, n, nums, dp);
+        int second = 0;
+        int third = 0;
+
+        if(start + 2 < n) {
+            int second = nums[start] + nums[start + 1] - find(start + 1, n, nums, dp);
+        }
+        if(start + 2 < n) {
+            int second = nums[start] + nums[start + 1] + nums[start + 2] - find(start + 3, n, nums, dp);
+        }
+        
+        return dp[start] = max(first, max(second, third));
+    }
+
+    string stoneGameIII(vector<int> &nums) {
+        int n = nums.size();
+
+        vector<int> dp(n, -1);
+
+        int ans = find(0, n, nums, dp);
+
+        if(ans == 0) return "Tie";
+        else if(ans > 0) return "Alice";
+
+        return "Bob";
+    }
+};
