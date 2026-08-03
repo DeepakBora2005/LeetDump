fc48d942aedc7a4546772028eeaf62650fe2ffd1
Deepak <boradeepakreddy2005@gmail.com>
2026-08-03 21:35:03 +0530

    Sync: 778. Swim in Rising Water (Hard) - Leet2Git

--- 778_Swim_in_Rising_Water.cpp
+++ 778_Swim_in_Rising_Water.cpp
@@ -1,52 +1,45 @@
 class Solution {
 public:
-    bool possible(int row, int col, int m, int n, vector<vector<int>> &grid, int mid, vector<vector<bool>> &visited) {
-        if(row >= m || row < 0 || col < 0 || col >= n) return false;
+    int dr[4] = {-1, 1, 0, 0};
+    int dc[4] = {0, 0, -1, 1};
+
+    bool possible(int row, int col, int n, vector<vector<int>> &grid, int mid, vector<vector<bool>> &visited) {
+        if(row >= n || row < 0 || col < 0 || col >= n) return false;
 
         if(grid[row][col] > mid) return false;
 
         if(visited[row][col]) return false;
 
-        if(row == m - 1 && col == n - 1) {
+        if(row == n - 1 && col == n - 1) {
             return true;
         }
 
         visited[row][col] = true;
 
-        if(possible(row + 1, col, m, n, grid, mid, visited)) {
-            return true;
-        }
+        for(int i = 0; i < 4; i++) {
+            int nr = row + dr[i];
+            int nc = col + dc[i];
 
-        if(possible(row - 1, col, m, n, grid, mid, visited)) {
-            return true;
-        }
-
-        if(possible(row, col + 1, m, n, grid, mid, visited)) {
-            return true;
-        }
-
-        if(possible(row, col - 1, m, n, grid, mid, visited)) {
-            return true;
+            if(possible(nr, nc, n, grid, mid, visited)) return true;
         }
 
         return false;
     }
 
     int swimInWater(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int n = grid.size();
 
         int low = 0;
-        int high = (m * n) - 1;
+        int high = (n * n) - 1;
 
         int ans = 0;
 
         while(low <= high) {
             int mid = low + (high - low) / 2;
 
-            vector<vector<bool>> visited(m, vector<bool>(n, false));
+            vector<vector<bool>> visited(n, vector<bool>(n, false));
 
-            if(possible(0, 0, m, n, grid, mid, visited)) {
+            if(possible(0, 0, n, grid, mid, visited)) {
                 ans = mid;
                 high = mid - 1;
             }
