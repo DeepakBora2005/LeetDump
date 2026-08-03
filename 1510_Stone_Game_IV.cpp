e334fbe4d2f278886bc153d227de11f1d7de7399
Deepak <boradeepakreddy2005@gmail.com>
2026-08-03 21:55:22 +0530

    Sync: 1510. Stone Game IV (Hard) - Leet2Git

---
+++ 1510_Stone_Game_IV.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool find(int n, vector<bool> &dp) {
+        if(n == 0) return false;
+
+        for(int i = 1; i <= n; i++) {
+            if(find(n - (i * i), dp) == false) return dp[n] = true;
+        }
+
+        return false;
+    }
+
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+
+        return find(n, dp);
+    }
+};
