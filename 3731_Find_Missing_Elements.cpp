e6c6212c980af62f18a4798c2a732b2184e3a9c9
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 10:27:31 +0530

    Sync: 3731. Find Missing Elements (Easy) - Leet2Git

--- 3731_Find_Missing_Elements.cpp
+++ 3731_Find_Missing_Elements.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        int low = 0;
-        int hi
+        int low = INT_MAX;
+        int high = INT_MIN;
+
+        unordered_map<int, int> mp;
+
+        for(int num : nums) {
+            low = min(low, num);
+            high = max(high, num);
+            mp[num]++;
+        }
 
         vector<int> ans;
 
-        for(int i = smallest; i <= largest; i++) {
+        for(int i = low; i <= high; i++) {
             if(mp[i] == 0) ans.push_back(i);
         }
 
