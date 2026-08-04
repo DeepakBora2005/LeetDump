cc6377329778e740c13a5e177ec89fa9b7f9846f
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 19:24:32 +0530

    Sync: 746. Min Cost Climbing Stairs (Easy) - Leet2Git

--- 746_Min_Cost_Climbing_Stairs.cpp
+++ 746_Min_Cost_Climbing_Stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
-    int find(int index, int n, vector<int> &cost) {
+    int find(int index, int n, vector<int> &cost, vector<int> &dp) {
         if(index >= n) return 0;
 
-        return cost[index] + min(find(index + 1, n, cost), find(index + 2, n, cost));
+        if(dp[index] != -1) return dp[index];
+
+        return dp[index] = cost[index] + min(find(index + 1, n, cost, dp), find(index + 2, n, cost, dp));
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
 
-        return min(find(0, n, cost), find(1, n, cost));
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+
+        return min(find(0, n, cost, dp1), find(1, n, cost, dp2));
     }
 };
