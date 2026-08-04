358b25168c64732239ae78289440703dcef3a7ee
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 20:22:40 +0530

    Sync: 120. Triangle (Medium) - Leet2Git

--- 120_Triangle.cpp
+++ 120_Triangle.cpp
@@ -3,11 +3,9 @@ public:
     int find(int row, int col, vector<vector<int>> &tri, vector<vector<int>> &dp) { 
         int m = tri.size();
 
-        if(row >= tri.size()) return INT_MIN;
-
         if(row == m - 1) return tri[row][col];
 
-        if(dp[row][col] != -1) return dp[row][col];
+        if(dp[row][col] != INT_MAX) return dp[row][col];
 
         int down = find(row + 1, col, tri, dp);
         int downRight = find(row + 1, col + 1, tri, dp);
@@ -18,7 +16,7 @@ public:
     int minimumTotal(vector<vector<int>>& tri) {
         int m = tri.size();
     
-        vector<vector<int>> dp(m, vector<int>(m, -1));
+        vector<vector<int>> dp(m, vector<int>(m, INT_MAX));
 
         return find(0, 0, tri, dp);
     }
