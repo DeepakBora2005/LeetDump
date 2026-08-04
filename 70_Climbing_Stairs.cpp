ba85c0c9c08b03becabd856fd6a32e512e208374
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 13:15:41 +0530

    Sync: 70. Climbing Stairs (Easy) - Leet2Git

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -1,8 +1,12 @@
 class Solution {
-public:
+public:     
+    vector<int> dp = vector<int>(46, -1);
+
     int climbStairs(int n) {
         if(n <= 1) return 1;
 
-        return climbStairs(n - 1) + climbStairs(n - 2);
+        if(dp[n] != -1) return dp[n];
+
+        return dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
