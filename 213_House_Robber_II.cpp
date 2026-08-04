02b2b76e6191247bc664f775939f0661a074ac92
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 13:41:18 +0530

    Sync: 213. House Robber II (Medium) - Leet2Git

---
+++ 213_House_Robber_II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int find(int index, vector<int> &nums, vector<int> &dp) {
+        if(index >= nums.size()) return 0;
+
+        if(dp[index] != -1) return dp[index];
+
+        int first = nums[index] + find(index + 2, nums, dp);
+        int second = find(index + 1, nums, dp);
+
+        return dp[index] = max(first, second);
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+
+        if(nums.size() == 1) return nums[0];
+
+        return find(0, nums, dp);
+    }
+};
