8ee335be143c9445a9de41aef68770d1309d133c
Deepak <boradeepakreddy2005@gmail.com>
2026-08-04 19:38:56 +0530

    Sync: 62. Unique Paths (Medium) - Leet2Git

--- 62_Unique_Paths.cpp
+++ 62_Unique_Paths.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    void find(int row, int col, int m, int n, int &ans) {
+    int find(int row, int col, int m, int n, vector<vector<int>> &dp) {
         if(row == m - 1 && col == n - 1) {
-            ans++;
+            return 1;
         }
 
-        if(row < 0 || row >= m || col < 0 || col >= n) {
-            return;
+        if(row >= m || col >= n) {
+            return 0;
         }
 
-        find(row + 1, col, m, n, ans);
-    
-        find(row, col + 1, m, n, ans);
+        if(dp[row][col] != -1) return dp[row][col];
+
+        return dp[row][col] = find(row + 1, col, m, n, dp) + find(row, col + 1, m, n, dp);
     }
 
     int uniquePaths(int m, int n) {
-        int ans = 0;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        find(0, 0, m, n, ans);
-
-        return ans;
+        return find(0, 0, m, n, dp);
     }
 };
