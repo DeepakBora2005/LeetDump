5f4db68f42a320675e8bc7bdc7997ade4a876eab
Deepak <boradeepakreddy2005@gmail.com>
2026-08-05 22:26:11 +0530

    Sync: 329. Longest Increasing Path in a Matrix (Hard) - Leet2Git

---
+++ 329_Longest_Increasing_Path_in_a_Matrix.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool find(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
+        if(j == p.size()) {
+            return i == s.size();
+        }
+
+        bool match = (i < s.size()) && (s[i] == p[j] || p[j] == '.');
+
+        if(j + 1 < p.size() && p[j + 1] == '*') {
+            return dp[i][j] = find(i, j + 2, s, p, dp) || (match && find(i + 1, j, s, p, dp));
+        }
+
+        if(match) return dp[i][j] = find(i + 1, j + 1, s, p, dp);
+
+        return dp[i][j] = false;
+    }
+
+    bool isMatch(string s, string p) {
+        int m = s.size();
+        int n = p.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+
+        return find(0, 0, s, p, dp);
+    }
+};
