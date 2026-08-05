241a1d4c4c43ea037b4568a14afbb638c3ad36c9
Deepak <boradeepakreddy2005@gmail.com>
2026-08-05 12:49:16 +0530

    Sync: 516. Longest Palindromic Subsequence (Medium) - Leet2Git

--- 516_Longest_Palindromic_Subsequence.cpp
+++ 516_Longest_Palindromic_Subsequence.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int find(int first, int second, string &text1, string &text2, vector<vector<int>> &dp) {
-        if(first == text1.size() || second == text2.size()) {
+    int find(int first, int second, string s1, string s2, vector<vector<int>> dp) {
+        if(first == s1.size() || second == s2.size()) {
             return 0;
         }
 
-        if(dp[first][second] != -1) return dp[first][second];
-
-        if(text1[first] == text2[second]) {
-            return dp[first][second] = 1 + find(first + 1, second + 1, text1, text2, dp);
+        if(dp[first][second] != -1) {
+            return dp[first][second];
         }
 
-        return dp[first][second] = max(find(first + 1, second, text1, text2, dp), find(first, second + 1, text1, text2, dp));
+        if(s1[first] == s2[second]) {
+            return dp[first][second] = 1 + find(first + 1, second + 1, s1, s2, dp);
+        }
+
+        return max(find(first + 1, second, s1, s2, dp), find(first, second + 1, s1, s2, dp));
     }
 
-    int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size();
-        int n = text2.size();
+    int longestPalindromeSubseq(string &s1) {
+        string s2 = s1;
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        int n = s1.size();
+        
+        vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        return find(0, 0, text1, text2, dp);
+        return find(0, 0, s1, s2, dp);
     }
 };
