610f2299f81686ef2b76a60a80613c6d888dc24c
Deepak <boradeepakreddy2005@gmail.com>
2026-08-05 12:52:11 +0530

    Sync: 516. Longest Palindromic Subsequence (Medium) - Leet2Git

--- 516_Longest_Palindromic_Subsequence.cpp
+++ 516_Longest_Palindromic_Subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int find(int first, int second, string s1, string s2, vector<vector<int>> dp) {
+    int find(int first, int second, string &s1, string &s2, vector<vector<int>> &dp) {
         if(first == s1.size() || second == s2.size()) {
             return 0;
         }
@@ -13,11 +13,12 @@ public:
             return dp[first][second] = 1 + find(first + 1, second + 1, s1, s2, dp);
         }
 
-        return max(find(first + 1, second, s1, s2, dp), find(first, second + 1, s1, s2, dp));
+        return dp[first][second] = max(find(first + 1, second, s1, s2, dp), find(first, second + 1, s1, s2, dp));
     }
 
-    int longestPalindromeSubseq(string &s1) {
+    int longestPalindromeSubseq(string s1) {
         string s2 = s1;
+        reverse(s2.begin(), s2.end());
 
         int n = s1.size();
         
