1fade2f99a205c3ea8611d07a71b206e330104ff
Deepak <boradeepakreddy2005@gmail.com>
2026-08-05 12:41:57 +0530

    Sync: 1143. Longest Common Subsequence (Medium) - Leet2Git

--- 1143_Longest_Common_Subsequence.cpp
+++ 1143_Longest_Common_Subsequence.cpp
@@ -5,11 +5,13 @@ public:
             return 0;
         }
 
+        if(dp[first][second] != -1) return dp[first][second];
+
         if(text1[first] == text2[second]) {
-            return 1 + find(first + 1, second + 1, text1, text2, dp);
+            return dp[first][second] = 1 + find(first + 1, second + 1, text1, text2, dp);
         }
 
-        return max(find(first + 1, second, text1, text2, dp), find(first, second + 1, text1, text2, dp));
+        return dp[first][second] = max(find(first + 1, second, text1, text2, dp), find(first, second + 1, text1, text2, dp));
     }
 
     int longestCommonSubsequence(string text1, string text2) {
