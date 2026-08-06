95d88f1d5a2aecc35505019e26a0a277a7ef7e93
Deepak <boradeepakreddy2005@gmail.com>
2026-08-06 20:57:31 +0530

    Sync: 72. Edit Distance (Medium) - Leet2Git

--- 72_Edit_Distance.cpp
+++ 72_Edit_Distance.cpp
@@ -30,6 +30,14 @@ public:
 
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = 0; i <= n; i++) {
+            dp[0][i] = 0;
+        }
+
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
                 if(word1[i - 1] == word2[j - 1]) {
