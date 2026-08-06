fa066c49f27a1917e48c50f22b79d4fa43747022
Deepak <boradeepakreddy2005@gmail.com>
2026-08-06 12:31:33 +0530

    Sync: 97. Interleaving String (Medium) - Leet2Git

--- 97_Interleaving_String.cpp
+++ 97_Interleaving_String.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    void find(int i, string &curr, string &s, unordered_set<string> &st) {
-        if(i == s.size()) {
-            if(!curr.empty()) {
-                st.insert(curr);
-            }
-            return;
+    int find(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<int>> &dp) {
+        if(i == s1.size() && j == s2.size()) {
+            if(k == s3.size()) return true;
+            else return false;
         }
 
-        curr.push_back(s[i]);
-        find(i + 1, curr, s, st);
-        curr.pop_back();
+        if(i < s1.size() && s1[i] == s3[k] && find(i + 1, j, k + 1, s1, s2, s3, dp)) {
+            return true;
+        }
         
-        find(i + 1, curr, s, st);        
+        if(j < s2.size() && s2[j] == s3[k] && find(i, j + 1, k + 1, s1, s2, s3, dp)) {
+            return true;
+        }
+
+        return false;
     }
 
-    int distinctSubseqII(string s) {
-        int n = s.size();
+    bool isInterleave(string s1, string s2, string s3) {
+        int n1 = s1.size();
+        int n2 = s2.size();
 
-        const int mod = 1e9 + 7;
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
 
-        unordered_set<string> st;
-
-        string curr = "";
-
-        find(0, curr, s, st);
-
-        int ans = st.size() % mod;
-
-        return ans;
+        return find(0, 0, 0, s1, s2, s3, dp);
     }
 };
