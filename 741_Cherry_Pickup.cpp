758aa35b2f6c4f76f7cc42bac0fa97f7759f9c5f
Deepak <boradeepakreddy2005@gmail.com>
2026-08-06 13:22:47 +0530

    Sync: 741. Cherry Pickup (Hard) - Leet2Git

--- 741_Cherry_Pickup.cpp
+++ 741_Cherry_Pickup.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void find(int i, int j, vector<vector<int>> &nums, vector<vector<int>> &dp, int &ans) {
+    void find(int i, int j, vector<vector<int>> &nums, vector<vector<bool>> &vis, int &ans) {
         int m = nums.size();
         int n = nums[0].size();
 
@@ -8,21 +8,20 @@ public:
 
         if(nums[i][j] == -1) return;
 
-        if(nums[i][j] == 1) ans++;
+        if(nums[i][j] == 1 && !vis[i][j]) ans++;
 
         if(nums[i][j] == 0 || nums[i][j] == 1) {
-            find(i + 1, j, nums, dp, ans);
+            find(i + 1, j, nums, vis, ans);
         
-            find(i, j + 1, nums, dp, ans);
+            find(i, j + 1, nums, vis, ans);
         }
-        
     }
 
     int cherryPickup(vector<vector<int>>& nums) {
         int m = nums.size();
         int n = nums[0].size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<bool>> dp(m, vector<bool>(n, false));
 
         int ans = 0;    
 
