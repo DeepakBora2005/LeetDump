c97386c2c0aa55df513d1eb010f9e6cc44083b9a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-07 19:28:01 +0530

    Sync: 115. Distinct Subsequences (Hard) - Leet2Git

--- 115_Distinct_Subsequences.cpp
+++ 115_Distinct_Subsequences.cpp
@@ -21,8 +21,19 @@ public:
         int m = s.size();
         int n = t.size();
 
-        vector<vector<int>> dp(m, vector<int>(n, -1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        return find(0, 0, s, t, dp);
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
