a748eaa82aef95d61afaec7f8d86cff28291779e
Deepak <boradeepakreddy2005@gmail.com>
2026-08-07 19:04:01 +0530

    Sync: 3348. Smallest Divisible Digit Product II (Hard) - Leet2Git

--- 3348_Smallest_Divisible_Digit_Product_II.cpp
+++ 3348_Smallest_Divisible_Digit_Product_II.cpp
@@ -12,6 +12,30 @@ public:
             x /= 10;
         }
 
+        long long c1, c2, c3, c4;
+
+        while(t % 2 == 0) {
+            c1++;
+            t /= 2;
+        }
+
+        while(t % 3 == 0) {
+            c2++;
+            t /= 3;
+        }
+
+        while(t % 5 == 0) {
+            c3++;
+            t /= 5;
+        }
+
+        while(t % 7 == 0) {
+            c4++;
+            t /= 7;
+        }
+
+        if(t != 1) return "-1";
+
         if(prod % 2 == 0) return num;
 
         return "-1";
