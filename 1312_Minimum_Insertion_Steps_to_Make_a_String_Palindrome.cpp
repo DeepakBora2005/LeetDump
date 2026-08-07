8b6a5696b8f0f355f3ae54cc0a67d3a79ffc6a2a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-07 22:52:20 +0530

    Sync: 1312. Minimum Insertion Steps to Make a String Palindrome (Hard) - Leet2Git

--- 1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.cpp
+++ 1312_Minimum_Insertion_Steps_to_Make_a_String_Palindrome.cpp
@@ -3,11 +3,13 @@ public:
     int find(int i, int j, string &s, string &t, vector<vector<int>> &dp) {
         if(i == s.size() || j == t.size()) return 0;
 
+        if(dp[i][j] != -1) return dp[i][j];
+
         if(s[i] == t[j]) {
-            return 1 + find(i + 1, j + 1, s, t, dp);
+            return dp[i][j] = 1 + find(i + 1, j + 1, s, t, dp);
         }
 
-        return max(find(i + 1, j, s, t, dp), find(i, j + 1, s, t, dp));
+        return dp[i][j] = max(find(i + 1, j, s, t, dp), find(i, j + 1, s, t, dp));
     }
 
     int minInsertions(string s) {
