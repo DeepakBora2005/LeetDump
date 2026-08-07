2ca700b3b4e2ac89651776e2f634e334f9b8eca1
Deepak <boradeepakreddy2005@gmail.com>
2026-08-07 23:35:59 +0530

    Sync: 583. Delete Operation for Two Strings (Medium) - Leet2Git

--- 583_Delete_Operation_for_Two_Strings.cpp
+++ 583_Delete_Operation_for_Two_Strings.cpp
@@ -6,10 +6,6 @@ public:
 
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for(int i = 0; i <= m; i++) dp[i][0] = 0;
-
-        for(int j = 0; j <= n; j++) dp[0][j] = 0;
-
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
                 if(word1[i] == word2[j]) {
