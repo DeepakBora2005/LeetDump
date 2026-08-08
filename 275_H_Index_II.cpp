d2cad29591d4a1d2e23d7553f1b7e730d9f9b539
Deepak <boradeepakreddy2005@gmail.com>
2026-08-08 20:34:58 +0530

    Sync: 275. H-Index II (Medium) - Leet2Git

---
+++ 275_H_Index_II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+
+        int low = 0;
+        int high = n - 1;
+
+        int ans = 0;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(citations[mid] > mid) {
+                high = mid - 1;
+                ans = (high - mid) + 1;
+            }
+            else if(citations[mid] <= mid) {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
