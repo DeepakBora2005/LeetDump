777bf5ad5d7106d49c31a718598a61cc4ea58a18
Deepak <boradeepakreddy2005@gmail.com>
2026-08-10 17:20:36 +0530

    Sync: 1563. Stone Game V (Hard) - Leet2Git

---
+++ 1563_Stone_Game_V.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int find(int index, int count, vector<int> &nums, vector<int> &prefix, vector<vector<int>> &dp) {
+        if(index >= nums.size() || count == 1) return 0;
+
+        int ans = 0;
+        for(int i = index; i < nums.size(); i++) {
+            int take = prefix[i] + find(i + 1, count - i, nums,prefix, dp);
+
+            ans = max(ans, take);
+        }
+
+        return ans;        
+    }
+
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+        vector<int> prefix(n, 0);
+        prefix[0] = stoneValue[0];
+
+        for(int i = 1; i < n; i++) {
+            prefix[i] = stoneValue[i] + prefix[i - 1];
+        }
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return find(0, n, stoneValue, prefix, dp);
+    }
+};
