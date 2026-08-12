de3117900a6c5a7d18db8d098fb4f33a2dc3bcc9
Deepak <boradeepakreddy2005@gmail.com>
2026-08-12 13:09:30 +0530

    Sync: 904. Fruit Into Baskets (Medium) - Leet2Git

--- 904_Fruit_Into_Baskets.cpp
+++ 904_Fruit_Into_Baskets.cpp
@@ -8,16 +8,21 @@ public:
 
         int ans = 0;
 
-        unordered_set<int> st;
+        unordered_map<int, int> mp;
  
         while(right < n) {
-            st.insert(fruits[right]);
+            mp[fruits[right]]++;
 
-            while(st.size() > 2) {
-                st.erase(fruits[left]);
+            while(mp.size() > 2) {
+                mp[fruits[left]]--;
+
+                if(mp[fruits[left]] == 0) {
+                    mp.erase(fruits[left]);
+                }
+
                 left++;
             }
-            
+
             ans = max(ans, right - left + 1);
 
             right++;
