d32528174daf8d3904e172191e0d03c3b959a9c5
Deepak <boradeepakreddy2005@gmail.com>
2026-08-12 09:14:58 +0530

    Sync: 44. Wildcard Matching (Hard) - Leet2Git

--- 44_Wildcard_Matching.cpp
+++ 44_Wildcard_Matching.cpp
@@ -1,34 +1,30 @@
 class Solution {
 public:
     bool find(int i, int j, string &s, string &p, vector<vector<int>> &dp) {
-        if(i == s.size() && j == p.size()) return true;
+        int m = s.size();
+        int n = p.size();
 
-        if(j == p.size()) return false;
-
-        if(i == s.size()) {
-            for(int k = j; k < p.size(); k++) {
-                if(p[k] != '*') return false;
+        if(i == m) {
+            for(int k = j; k < n; k++) {
+                if(p[k] != '*') return 0;
             }
 
-            return true;
+            return 1;
+        } 
+
+        if(j == n) {
+            if(i == m) return 1;    
+            else return 0;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
+        return false;
+    }
 
-        if(s[i] == p[j] || p[j] == '?') return dp[i][j] = find(i + 1, j + 1, s, p, dp);
-
-        if(p[j] == '*') {
-            return dp[i][j] = find(i + 1, j, s, p, dp) || find(i, j + 1, s, p, dp);
-        }
-
-        return dp[i][j] = false;
-    } 
-    
     bool isMatch(string s, string p) {
         int m = s.size();
         int n = p.size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        vector<vector<int>> dp(m, vector<int>(n, -1));
 
         return find(0, 0, s, p, dp);
     }
