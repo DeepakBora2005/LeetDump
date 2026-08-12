66718160b157676206c4e422a054201df960e9a7
Deepak <boradeepakreddy2005@gmail.com>
2026-08-12 09:05:15 +0530

    Sync: 2958. Length of Longest Subarray With at Most K Frequency (Medium) - Leet2Git

--- 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
+++ 2958_Length_of_Longest_Subarray_With_at_Most_K_Frequency.cpp
@@ -5,26 +5,19 @@ public:
 
         int n = nums.size();
 
-        vector<int> temp(n, 0);
-
-        for(int i = 0; i < n; i++) {
-            mp[nums[i]]++;
-
-            temp[i] = mp[nums[i]];
-        }
-
         int ans = 0;
         int left = 0;
         int right = 0;
 
         while(right < n) {
-            if(temp[right] <= k) {
-                ans = max(ans, (right - left) + 1);
-            }
-            else if(temp[right] > k) {
+            mp[nums[right]]++;
+
+            while(mp[nums[right]] > k) {
+                mp[nums[left]]--;
                 left++;
-                ans = max(ans, (right - left) + 1);
             }
+
+            ans = max(ans, right - left + 1);
 
             right++;
         }
