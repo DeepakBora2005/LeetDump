0462af03067a71563d465e5cfaed7a2fcfcfc360
Deepak <boradeepakreddy2005@gmail.com>
2026-08-14 10:31:36 +0530

    Sync: 55. Jump Game (Medium) - Leet2Git

--- 55_Jump_Game.cpp
+++ 55_Jump_Game.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool find(int index, int n, vector<int> &nums, vector<int> &dp) {
-        if(index == n) return true;
+        if(index >= n) return true;
 
         if(nums[index] == 0) return false;
-
-        if(index + nums[index] > n) return false;
 
         return find(index + 1, n, nums, dp) || find(index + nums[index], n, nums, dp);
 
