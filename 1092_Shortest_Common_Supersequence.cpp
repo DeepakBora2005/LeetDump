4f50a55eb0e6a1695f2f9b8d355ad0bfda8e2ecd
Deepak <boradeepakreddy2005@gmail.com>
2026-08-14 21:05:01 +0530

    Sync: 1092. Shortest Common Supersequence  (Hard) - Leet2Git

---
+++ 1092_Shortest_Common_Supersequence.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
+        int m = str1.size();
+        int n = str2.size();
+        
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(str1[i - 1] == str2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        int i = m;
+        int j = n;
+
+        int temp = m + n - dp[m][n];
+
+        char ans[temp];
+
+        int k = temp - 1;
+
+        while(i >= 0 && j >= 0 && k >= 0) {
+            if(str1[i] == str2[j]) {
+                ans[k--] = str1[i];
+            }
+            else if(str1[i] != str2[j]) {
+                ans[k--] = str1[i];
+            }
+
+            if(dp[i - 1][j] > dp[i][j - 1]) {
+                i--;
+            }
+            else if(dp[i][j - 1] > dp[i - 1][j]) {
+                j--;
+            }
+            else {
+                j--;
+            }
+        }
+
+        return ans;
+    }
+};
