69d65b3fe9e086eb1d2723d580e2c87f76d64e4e
Deepak <boradeepakreddy2005@gmail.com>
2026-08-14 21:15:52 +0530

    Sync: 1092. Shortest Common Supersequence  (Hard) - Leet2Git

--- 1092_Shortest_Common_Supersequence.cpp
+++ 1092_Shortest_Common_Supersequence.cpp
@@ -20,37 +20,35 @@ public:
         int i = m;
         int j = n;
 
-        int temp = m + n - dp[m][n];
-
-        string ans(temp, ' ');
-
-        int k = temp - 1;
+        string ans = "";
 
         while(i > 0 && j > 0) {
             if(str1[i - 1] == str2[j - 1]) {
-                ans[k--] = str1[i - 1];
+                ans += str1[i - 1];
                 i--;
                 j--;
             }
             else if(dp[i - 1][j] > dp[i][j - 1]) {
-                ans[k--] = str1[i - 1];
+                ans += str1[i - 1];
                 i--;
             }
             else {
-                ans[k--] = str2[j - 1];
+                ans += str2[j - 1];
                 j--;
             }
         }
 
         while(j > 0) {
-            ans[k--] = str2[j - 1];
+            ans += str2[j - 1];
             j--;
         }
 
         while(i > 0) {
-            ans[k--] = str1[i - 1];
+            ans += str1[i - 1];
             i--;
         } 
+
+        reverse(ans.begin(), ans.end());
 
         return ans;
     }
