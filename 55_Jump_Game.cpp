70391dc2f68ef7f6c6bb390ad3a5b9968f802d04
Deepak <boradeepakreddy2005@gmail.com>
2026-08-14 10:27:45 +0530

    Sync: 55. Jump Game (Medium) - Leet2Git

---
+++ 55_Jump_Game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool find(int index, int n, vector<int> &nums, vector<int> &dp) {
+        if(index == n) return true;
+
+        if(nums[index] == 0) return false;
+
+        if(index + nums[index] > n) return false;
+
+        return find(index + 1, n, nums, dp) || find(index + nums[index], n, nums, dp);
+
+        return false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> dp(n, -1);
+
+        return find(0, n - 1, nums, dp);
+    }
+};
