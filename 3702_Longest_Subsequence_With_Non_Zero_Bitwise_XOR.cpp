8032c1d60774df6da221f212da15aff4bb1db641
Deepak <boradeepakreddy2005@gmail.com>
2026-08-15 08:56:57 +0530

    Sync: 3702. Longest Subsequence With Non-Zero Bitwise XOR (Medium) - Leet2Git

--- 3702_Longest_Subsequence_With_Non_Zero_Bitwise_XOR.cpp
+++ 3702_Longest_Subsequence_With_Non_Zero_Bitwise_XOR.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    int find(int i, int curr, vector<int> &nums, vector<vector<int>> &dp) {
-        int n = nums.size();
-
-        if(i == n) return 0;
-
-        int take = 0;
-        int notTake = 0;
-        if(curr ^ nums[i] != 0) {
-            take = 1 + find(i + 1, curr ^ nums[i], nums, dp);
-        }
-
-        notTake = find(i + 1, curr, nums, dp);
-
-
-        return max(take, notTake);
-    }
-
     int longestSubsequence(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(2, -1));
+        bool hasNonZero = false;
+        int count = 0;
 
-        return find(0, 0, nums, dp);
+        int curr = 0;
+
+        for(int i = 0; i < n; i++) {
+            curr ^= nums[i];
+            
+            if(nums[i] != 0) hasNonZero = true;
+        }
+
+        if(curr != 0) return n;
+
+        if(hasNonZero) return n - 1;
+
+        return 0;
     }
 };
