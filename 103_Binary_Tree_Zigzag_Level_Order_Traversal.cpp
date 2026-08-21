51595ce5762bc09769e8b00e37be0962fd587a99
Deepak <boradeepakreddy2005@gmail.com>
2026-08-21 20:35:10 +0530

    Sync: 103. Binary Tree Zigzag Level Order Traversal (Medium) - Leet2Git

---
+++ 103_Binary_Tree_Zigzag_Level_Order_Traversal.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+
+        if(root == nullptr) return ans;
+        queue<TreeNode*> q;
+
+        q.push(root);
+        int bit = 0;
+        while(!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for(int i = 0; i < size; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+
+                if(node->left != nullptr) q.push(node->left);
+                if(node->right != nullptr) q.push(node->right);
+
+                level.push_back(node->val); 
+            }
+
+            if(bit == 0) {
+                ans.push_back(level);
+                bit = 1;
+            }
+            else {
+                reverse(level.begin(), level.end());
+                ans.push_back(level);
+            }
+            
+        }
+
+        return ans;    
+    }
+};
