4184eea58dd1720e40202a2e672c9ee58b02dcb3
Deepak <boradeepakreddy2005@gmail.com>
2026-08-22 11:51:17 +0530

    Sync: 678. Valid Parenthesis String (Medium) - Leet2Git

---
+++ 678_Valid_Parenthesis_String.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0;
+        int leftMax = 0;
+
+        int n = s.length();
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] == '(') {
+                leftMin++;
+                leftMax++;
+            }
+            else if(s[i] == ')') {
+                leftMin--;
+                leftMax--;
+            }
+            else {
+                leftMin--;
+                leftMax++;
+            }
+
+            if(leftMin < 0) leftMin = 0;
+            if(leftMax < 0) return false;
+        }
+
+        return true;
+    }
+};
