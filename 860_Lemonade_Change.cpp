de5722010e86458753358ccb49345a29f19ca229
Deepak <boradeepakreddy2005@gmail.com>
2026-08-22 11:41:15 +0530

    Sync: 860. Lemonade Change (Easy) - Leet2Git

---
+++ 860_Lemonade_Change.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int n = bills.size();
+
+        int count5 = 0;
+        int count10 = 0;
+        int count20 = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(bills[i] == 5) {
+                count5++;
+            } 
+            else {
+                if(bills[i] == 10) {
+                    count10++;
+                    if(count5 > 0) {
+                        count5--;
+                    }
+                    else return false;
+                }
+                else if(bills[i] == 20) {
+                    if(count10 > 0) {
+                        count10--;
+                    }
+                    if(count5 > 0) {
+                        count5--;
+                    }
+                    if(count5 == 0 && count10 == 0) return false;
+                }
+            }
+        }
+
+
+        return true;
+    }
+};
