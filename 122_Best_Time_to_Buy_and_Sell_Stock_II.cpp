148a74a6375c1a558db511162bb2bcb006b22c6d
Deepak <boradeepakreddy2005@gmail.com>
2026-08-23 13:37:23 +0530

    Sync: 122. Best Time to Buy and Sell Stock II (Medium) - Leet2Git

--- 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -4,23 +4,12 @@ public:
         int n = prices.size();
 
         int maxProfit = 0;
-        int minCost = prices[0];
-        int maxCost = prices[0];
-
-        for(int i = 0; i < n; i++) {
-            int currentCost = prices[i];
-            
-            if(currentCost > maxCost) {
-                maxCost = currentCost;
-            }
-            else if(currentCost < maxCost) {
-                maxProfit += (maxCost - minCost);
-                minCost = currentCost;
-                maxCost = currentCost;
+        
+        for(int i = 1; i < n; i++) {
+            if(prices[i] > prices[i - 1]) {
+                maxProfit += (prices[i] - prices[i - 1]);
             }
         }
-
-        if(maxCost != minCost) maxProfit += maxCost - minCost;
 
         return maxProfit;
     }
