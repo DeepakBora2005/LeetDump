c0fdde7350fa8a9a20639a2fc68743c299e20835
Deepak <boradeepakreddy2005@gmail.com>
2026-08-23 13:45:39 +0530

    Sync: 368. Largest Divisible Subset (Medium) - Leet2Git

---
+++ 368_Largest_Divisible_Subset.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void find(int i, vector<int> &nums, vector<int> &ans) {
+        int n = nums.size();
+
+        if(i >= n) return;
+
+        if(i + 1 < n) {
+            if((nums[i] % nums[i + 1] == 0) || (nums[i + 1] % nums[i] == 0)) {
+                ans.push_back(i);
+                ans.push_back(i + 1);
+
+                return;
+            }
+
+            find(i + 1, nums, ans);
+        }
+    }
+
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        vector<int> ans;
+
+        find(0, nums, ans);
+
+        return ans;
+    }
+};
