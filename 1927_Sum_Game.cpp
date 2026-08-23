ffecb1c068083463033235dc7fb2818cdf33086f
Deepak <boradeepakreddy2005@gmail.com>
2026-08-23 11:37:22 +0530

    Sync: 1927. Sum Game (Medium) - Leet2Git

--- 1927_Sum_Game.cpp
+++ 1927_Sum_Game.cpp
@@ -5,10 +5,19 @@ public:
 
         int leftCount = 0;
         int rightCount = 0;
+        int leftSum = 0;
+        int rightSum = 0;
 
         for(int i = 0; i < n / 2; i++) {
             if(num[i] == '?') leftCount++;
+            else if(num[i] != '?') leftSum += (int)num[i];
+
             if(num[n / 2 + i] == '?') rightCount++;
+            else if(num[n / 2 + i] != '?') rightSum += (int)num[n / 2 + i];
+        }
+
+        if(abs(rightCount - leftCount) == 0) {
+            if(leftSum == rightSum) return true;
         }
 
         if(abs(rightCount - leftCount) > 0) return true;
