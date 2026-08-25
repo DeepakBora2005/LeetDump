fc2a44cebfbf4dd28ec54f6f50ff7cfed40fba8e
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 21:20:34 +0530

    Sync: 2140. Solving Questions With Brainpower (Medium) - Leet2Git

---
+++ 2140_Solving_Questions_With_Brainpower.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int find(int index, vector<int> &nums, int target, vector<int> &dp) {
+        int n  = nums.size();
+
+        if(index >= n) return -1;
+
+        if(index == n - 1) return 0;
+
+        if(dp[index] != INT_MIN) return dp[index];
+
+        int mini = -target;
+        int maxi = target;
+
+        int ans = -1;
+        for(int j = 1; j < n; j++) {
+            int temp = -1;
+            if((index + j < n) && (nums[index + j] - nums[index] <= maxi) && (nums[index + j] - nums[index] >= mini)) {
+                temp = find(index + j, nums, target, dp);
+
+                if(temp != -1) {
+                    ans = max(ans, 1 + temp);
+                }
+            }
+        }
+
+        return dp[index] = ans;
+    }
+
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> dp(n, INT_MIN);
+
+        return find(0, nums, target, dp);
+    }
+};
