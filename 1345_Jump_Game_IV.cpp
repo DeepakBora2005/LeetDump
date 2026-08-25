8aaf1798d3352086b0641fa409ab39db485df41b
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 11:27:45 +0530

    Sync: 1345. Jump Game IV (Hard) - Leet2Git

---
+++ 1345_Jump_Game_IV.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int minJumps(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n == 1) {
+            return 0;
+        }
+
+        unordered_map<int, vector<int>> mp;
+
+        for(int i = 0;  i < n; i++) {
+            mp[nums[i]].push_back(i);
+        }
+
+        queue<int> q;
+        vector<int> dist(n, -1);
+
+        q.push(0);
+        dist[0] = 0;
+
+        while(!q.empty()) {
+            int i = q.front();
+            q.pop();
+
+            int d = dist[i];
+
+            if(i == n - 1) return d;
+
+            if(i - 1 >= 0 && dist[i - 1] == -1) {
+                dist[i - 1] = d + 1;
+                q.push(i - 1);
+            }
+
+            if(i + 1 < n && dist[i + 1] == -1) {
+                dist[i + 1] == d + 1;
+                q.push(i + 1);
+            }
+
+            if(mp.count(nums[i])) {
+                for(int j : mp[nums[i]]) {
+                    if(dist[j] == -1) {
+                        dist[j] = d + 1;
+                        q.push(j);
+                    }
+                }
+
+                mp[nums[i]].clear();
+            }
+        }
+
+        return -1;
+    }
+};
