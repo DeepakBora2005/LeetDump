bb65d06fd8ea5b4f0ba68f774288a8ffdb44e699
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 22:45:09 +0530

    Sync: 1824. Minimum Sideway Jumps (Medium) - Leet2Git

--- 1824_Minimum_Sideway_Jumps.cpp
+++ 1824_Minimum_Sideway_Jumps.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    int find(int index, int lane, vector<int> &nums) {
+    int find(int index, int lane, vector<int> &nums, vector<vector<int>> &dp) {
         int n = nums.size();
 
         if(index >= n) {
             return 0;
         }
 
+        if(dp[index][lane] != -1) return dp[index][lane];
+
         if(nums[index + 1] != lane) {
-            return find(index + 1, lane, nums);
+            return dp[index][lane] = find(index + 1, lane, nums, dp);
         }
 
         int ans = INT_MAX;
 
         if(lane != 1 && nums[index] != 1) {
-            ans = min(ans, 1 + find(index, 1, nums));
+            ans = min(ans, 1 + find(index, 1, nums, dp));
         }
         if(lane != 2 && nums[index] != 2) {
-            ans = min(ans, 1 + find(index, 2, nums));
+            ans = min(ans, 1 + find(index, 2, nums, dp));
         }
         if(lane != 3 && nums[index] != 3) {
-            ans = min(ans, 1 + find(index, 3, nums));
+            ans = min(ans, 1 + find(index, 3, nums, dp));
         }
 
-        return ans;
+        return dp[index][lane] = ans;
     }
 
     int minSideJumps(vector<int>& nums) {
-        return find(0, 2, nums);
+        int n = nums.size();
+
+        vector<vector<int>> dp(n, vector<int>(4, -1));
+
+        return find(0, 2, nums, dp);
     }
 };
