d91d8514a29c372dc0558527834aa7676ec8406b
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 10:48:48 +0530

    Sync: 1690. Stone Game VII (Medium) - Leet2Git

--- 1690_Stone_Game_VII.cpp
+++ 1690_Stone_Game_VII.cpp
@@ -5,10 +5,10 @@ public:
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        int leftSum = prefix[j + 1] - prefix[i + 1];
+        int leftSum = prefix[j] - prefix[i];
         int leftMost = leftSum - find(i + 1, j, nums, prefix, dp);
 
-        int rightSum = prefix[j] - prefix[i];
+        int rightSum = prefix[j - 1] - (i > 0 ? prefix[i - 1] : 0);
         int rightMost = rightSum - find(i, j - 1, nums, prefix, dp);
 
         return dp[i][j] = max(leftMost, rightMost);
@@ -19,10 +19,11 @@ public:
 
         vector<vector<int>> dp(n, vector<int>(n, -1));
 
-        vector<int> prefix(n + 1, 0);
+        vector<int> prefix(n);
+        prefix[0] = stones[0];
 
-        for(int i = 0; i < n; i++) {
-            prefix[i + 1] = stones[i] + prefix[i];
+        for(int i = 1; i < n; i++) {
+            prefix[i] = stones[i] + prefix[i - 1];
         }
 
         return find(0, n - 1, stones, prefix, dp);
