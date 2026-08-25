eac03f66470051aa03d4bbe4239f7ef429b34bce
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 22:43:57 +0530

    Sync: 1824. Minimum Sideway Jumps (Medium) - Leet2Git

--- 1824_Minimum_Sideway_Jumps.cpp
+++ 1824_Minimum_Sideway_Jumps.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    long long find(long long index, vector<vector<int>> &nums, vector<long long> &dp) {
-        int n = nums.size(); 
+    int find(int index, int lane, vector<int> &nums) {
+        int n = nums.size();
 
-        if(index >= n) return 0;
+        if(index >= n) {
+            return 0;
+        }
 
-        if(dp[index] != -1) return dp[index];
+        if(nums[index + 1] != lane) {
+            return find(index + 1, lane, nums);
+        }
 
-        long long skip = nums[index][1];
-        long long take = nums[index][0] + find(index + skip + 1, nums, dp);
-        
-        long long notTake = find(index + 1, nums, dp);
+        int ans = INT_MAX;
 
-        return dp[index] = max(take, notTake);
+        if(lane != 1 && nums[index] != 1) {
+            ans = min(ans, 1 + find(index, 1, nums));
+        }
+        if(lane != 2 && nums[index] != 2) {
+            ans = min(ans, 1 + find(index, 2, nums));
+        }
+        if(lane != 3 && nums[index] != 3) {
+            ans = min(ans, 1 + find(index, 3, nums));
+        }
+
+        return ans;
     }
 
-    long long mostPoints(vector<vector<int>>& nums) {
-        long long n = nums.size();
-
-        vector<long long> dp(n, -1);
-
-        return find(0, nums, dp);
+    int minSideJumps(vector<int>& nums) {
+        return find(0, 2, nums);
     }
 };
