d8c1c3d45dd4bc576a0388072a28f9cbe00c259c
Deepak <boradeepakreddy2005@gmail.com>
2026-08-25 09:52:53 +0530

    Sync: 1872. Stone Game VIII (Hard) - Leet2Git

--- 1872_Stone_Game_VIII.cpp
+++ 1872_Stone_Game_VIII.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
-    int find(int index, int turn, vector<int> &nums, vector<int> &prefix) {
+    int find(int index, vector<int> &nums, vector<int> &prefix) {
         int n = nums.size();
 
-        if(index >= n) return 0;
-
-        int score1 = 0;
-        int score2 = 0;
-
-        if(turn == 0) {
-            for(int i = 2; i < n; i++) {
-                int take = 0;
-                if(index + i < n) {
-                    take = prefix[i + 1] + find(index + i, 1, nums, prefix); 
-                }
-
-                score1 = max(score1, take);
-            }
-        }
-        else {
-            for(int i = 2; i < n; i++) {
-                int take = 0;
-                if(index + i < n) {
-                    take = prefix[i + 1] + find(index + i, 0, nums, prefix);
-                }
-
-                score2 = min(score2, take);
-            }
+        if(index == n - 1) {
+            return prefix[index];
         }
 
-        return score1 - score2;
+        int ans = INT_MIN;
+
+        for(int j = index + 1; j < n; j++) {
+            ans = max(ans, prefix[index] - find(j, nums, prefix));
+        }
+
+        return ans;
     }
 
     int stoneGameVIII(vector<int>& stones) {
         int n = stones.size();
 
         vector<int> prefix(n + 1, 0);
+
         for(int i = 0; i < n; i++) {
             prefix[i + 1] = stones[i] + prefix[i];
         }
 
-        return find(0, 0, stones, prefix);
+        return find(0, stones, prefix);
     }
 };
