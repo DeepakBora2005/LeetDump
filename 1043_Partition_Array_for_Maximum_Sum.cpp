30b2b42d95daf2fcc66480f826ac9f9e83eaf5e0
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 23:36:43 +0530

    Sync: 1043. Partition Array for Maximum Sum (Medium) - Leet2Git

---
+++ 1043_Partition_Array_for_Maximum_Sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public: 
+    int find(int i, int k, vector<int> &nums) {
+        int n = nums.size();
+
+        if(i == n) return 0;
+
+        int maxi = INT_MIN;
+        int len = 0;
+        int ans = 0;
+
+        for(int j = i; j < min(n, j + k); j++) {
+            len++;
+            maxi = max(maxi, nums[j]);
+            int sum = (len * maxi) + find(j + 1, k, nums);
+
+            ans = max(ans, sum);
+        }
+
+        return ans;
+    }
+
+    int maxSumAfterPartitioning(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        return find(0, k, nums);
+    }
+};
