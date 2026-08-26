4fab1bdfb44e012b1eadecd868317057a9bab0b7
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 13:11:27 +0530

    Sync: 3840. House Robber V (Medium) - Leet2Git

--- 3840_House_Robber_V.cpp
+++ 3840_House_Robber_V.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    long long find(int i, int j, vector<int> &a, vector<int> &b) {
-        if(i == a.size() || j == b.size()) {
-            return 0;
+    long long find(int i, vector<int> &a, vector<int> &b) {
+        if(i >= a.size()) return 0;
+
+        long long skip = find(i + 1, a, b);
+
+        long long take = 0;
+
+        if(i + 1 < a.size() && b[i] == b[i + 1]) {
+            take = a[i] + find(i + 2, a, b);
         }
-
-        int skip = 0;
-        if(a[i] == b[j]) {
-            skip = find(i + 1, j + 1, a, b);
+        else {
+            take = a[i] + find(i + 1, a, b);
         }
-
-        int take = a[i] + find(i + 1, j + 1, a, b);
-
+        
         return max(skip, take);
     }
 
     long long rob(vector<int>& nums, vector<int>& colors) {
         int n = nums.size();
 
-        return find(0, 0, nums, colors);
+        return find(0, nums, colors);
     }
 };
