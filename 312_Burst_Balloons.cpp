0887b698252b6609220f3992c7eb1146f84c335a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 22:28:58 +0530

    Sync: 312. Burst Balloons (Hard) - Leet2Git

--- 312_Burst_Balloons.cpp
+++ 312_Burst_Balloons.cpp
@@ -16,10 +16,10 @@ public:
     }
 
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-
         nums.insert(nums.begin(), 1);
         nums.push_back(1);
+
+        int n = nums.size();
 
         return find(1, n - 2, nums);
     }
