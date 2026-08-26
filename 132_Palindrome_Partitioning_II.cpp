52e9b456d34614eeb72b348bf879e34b259335d2
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 23:20:29 +0530

    Sync: 132. Palindrome Partitioning II (Hard) - Leet2Git

--- 132_Palindrome_Partitioning_II.cpp
+++ 132_Palindrome_Partitioning_II.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int find(int i, string &s) {    
+    int find(int i, string &s, vector<int> &dp) {    
         int n = s.size();
 
         if(i == n) return 0;
+
+        if(dp[i] != -1) return dp[i];
+
         int minCost = INT_MAX;
 
         for(int j = i; j < n; j++) {
             if(isValid(s, i, j)) {
-                int cost = 1 + find(j + 1, s);
+                int cost = 1 + find(j + 1, s, dp);
 
                 minCost = min(minCost, cost);
             }
         }
 
-        return minCost;
+        return dp[i] = minCost;
     } 
 
     bool isValid(string &s, int i, int j) {
@@ -32,7 +35,9 @@ public:
 
     int minCut(string s) {
         int n = s.size();
+        
+        vector<int> dp(n, -1);
 
-        return find(0, s) - 1;
+        return find(0, s, dp) - 1;
     }
 };
