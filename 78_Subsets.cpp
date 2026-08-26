2563f06b6a0b8e7d0b83d17520aea4365b4cdb46
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 11:44:28 +0530

    Sync: 78. Subsets (Medium) - Leet2Git

---
+++ 78_Subsets.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void find(int index, int n, vector<int> &nums, vector<int> &temp, vector<vector<int>> &result) {
+        if(index == n) {
+            result.push_back(temp);
+            return;
+        }
+        
+        temp.push_back(nums[index]);
+        find(index + 1, n, nums, temp, result);
+        temp.pop_back();
+        find(index + 1, n, nums, temp, result);
+    }
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> result;
+        vector<int> temp;
+
+        for(int mask = 0; mask < (1 << n); mask++) {
+
+            for(int i = 0; i < n; i++) {
+                if(mask & (1 << i)) {
+                    temp.push_back(nums[i]);
+                }
+            }
+
+            result.push_back(temp);
+        }
+
+        return result;
+    }
+};
