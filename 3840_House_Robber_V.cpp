65a1dc49c6578bd28a725fddb46b67103b274eac
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 13:05:00 +0530

    Sync: 3840. House Robber V (Medium) - Leet2Git

---
+++ 3840_House_Robber_V.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long find(int i, int j, vector<int> &a, vector<int> &b) {
+        if(i == a.size() || j == b.size()) {
+            return 0;
+        }
+
+        int skip = 0;
+        if(a[i] == b[j]) {
+            skip = max(find(i + 1, j, a, b), find(i, j + 1, a, b));
+        }
+
+        int take = a[i] + max(find(i + 1, j, a, b), find(i, j + 1, a, b));
+
+
+        return max(skip, take);
+    }
+
+    long long rob(vector<int>& nums, vector<int>& colors) {
+        int n = nums.size();
+
+        return find(0, 0, nums, colors);
+    }
+};
