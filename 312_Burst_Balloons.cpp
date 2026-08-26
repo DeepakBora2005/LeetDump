b4b04a6e31378343bd9e3856dae30dfbca6cd07a
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 23:01:08 +0530

    Sync: 312. Burst Balloons (Hard) - Leet2Git

--- 312_Burst_Balloons.cpp
+++ 312_Burst_Balloons.cpp
@@ -23,8 +23,21 @@ public:
 
         int n = nums.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, -1));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        return find(1, n - 2, nums, dp);
+        for(int i = n - 2; i >= 1; i--) {
+            for(int j = i; j <= n - 2; j++) {
+
+                dp[i][j] = INT_MIN;
+
+                for(int k = i; k <= j; k++) {
+                    int cost = nums[i - 1] * nums[k] * nums[j + 1] + dp[i][k - 1] + dp[k + 1][j];
+
+                    dp[i][j] = max(dp[i][j], cost);
+                }
+            }
+        }
+
+        return dp[1][n - 2];
     }
 };
