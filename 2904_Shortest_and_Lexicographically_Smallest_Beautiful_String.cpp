088ff8e0dab1f2a19eff0e9f953e800e905e6796
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 09:21:36 +0530

    Sync: 2904. Shortest and Lexicographically Smallest Beautiful String (Medium) - Leet2Git

---
+++ 2904_Shortest_and_Lexicographically_Smallest_Beautiful_String.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+
+
+        int left = 0;
+        int right = 0;
+        int ans = n;
+        int count = 0;
+
+        int start = 0;
+        int end = 0;
+        
+        string result = "";
+
+        while(right < n) {
+            if(s[right] == 1) {
+                count++;
+            }
+
+            while(count > k) {
+                if(s[left] == '1') {
+                    count--;
+                }
+
+                left++;
+            }
+
+            if(count == k) {
+                if(ans < right - left + 1) {
+                    start = left;
+                    end = right;
+                }
+            }
+
+            right++;
+        }
+
+        for(int i = left; i <= right; i++) {
+            result += s[i];
+        }
+
+        return result;
+    }
+};
