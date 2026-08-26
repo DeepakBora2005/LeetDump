bca9a7132f60828937b7bf1b6e20135de79d5273
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 11:45:13 +0530

    Sync: 78. Subsets (Medium) - Leet2Git

--- 78_Subsets.cpp
+++ 78_Subsets.cpp
@@ -16,10 +16,10 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> result;
-        vector<int> temp;
 
         for(int mask = 0; mask < (1 << n); mask++) {
-
+            vector<int> temp;
+            
             for(int i = 0; i < n; i++) {
                 if(mask & (1 << i)) {
                     temp.push_back(nums[i]);
