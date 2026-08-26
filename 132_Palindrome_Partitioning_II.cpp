31e7df9b79214db5a373c76dc867bbedd75111d6
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 23:26:17 +0530

    Sync: 132. Palindrome Partitioning II (Hard) - Leet2Git

--- 132_Palindrome_Partitioning_II.cpp
+++ 132_Palindrome_Partitioning_II.cpp
@@ -37,18 +37,19 @@ public:
         int n = s.size();
         
         vector<int> dp(n + 1, 0);
+        dp[n] = -1;
 
         for(int i = n - 1; i >= 0; i--) {
 
             int minCuts = INT_MAX;
 
-            for(int j = 0; j < n; j++) {
+            for(int j = i; j < n; j++) {
                 if(isValid(s, i, j)) {
-                    int minCuts = min(minCuts, 1 + dp[j + 1]);
+                    minCuts = min(minCuts, 1 + dp[j + 1]);
                 }
-
-                dp[i] = minCuts;
             }
+
+            dp[i] = minCuts;
         }
 
         return dp[0];
