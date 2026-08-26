4cf644232262b95a4a43a8bb5a2166985a52321f
Deepak <boradeepakreddy2005@gmail.com>
2026-08-26 22:30:29 +0530

    Sync: 312. Burst Balloons (Hard) - Leet2Git

--- 312_Burst_Balloons.cpp
+++ 312_Burst_Balloons.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int find(int i, int j, vector<int> &nums) {
+    int find(int i, int j, vector<int> &nums, vector<vector<int>> &dp) {
         int n = nums.size();
+
+        if(dp[i][j] != -1) return dp[i][j];
 
         if(i > j) return 0;
 
         int ans = 0;
         for(int k = i; k <= j; k++) {
-            int cost =  nums[i - 1] * nums[k] * nums[j + 1] + find(i, k - 1, nums) + find(k + 1, j, nums);
+            int cost =  nums[i - 1] * nums[k] * nums[j + 1] + find(i, k - 1, nums, dp) + find(k + 1, j, nums, dp);
 
             ans = max(ans, cost);
         }
 
-        return ans;
+        return dp[i][j] = ans;
     }
 
     int maxCoins(vector<int>& nums) {
@@ -21,6 +23,8 @@ public:
 
         int n = nums.size();
 
-        return find(1, n - 2, nums);
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return find(1, n - 2, nums, dp);
     }
 };
