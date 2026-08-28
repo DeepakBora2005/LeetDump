12c1d9fb9844e7b4ef0e8d102e9dadb54a363a5f
Deepak <boradeepakreddy2005@gmail.com>
2026-08-28 22:46:39 +0530

    Sync: 121. Best Time to Buy and Sell Stock (Easy) - Leet2Git

--- 121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -11,7 +11,7 @@ public:
             int profit = 0;
 
             if(nums[j] > nums[i]) {
-                profit = nums[j] - nums[i] + find(i + 1, nums);
+                profit = nums[j] - nums[i] + find(j + 1, nums);
             }
 
             maxProfit = max(profit, maxProfit);
