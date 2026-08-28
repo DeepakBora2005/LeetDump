9812ee7fb1e12e43de94765a919c4bd8e9874ace
Deepak <boradeepakreddy2005@gmail.com>
2026-08-28 23:00:32 +0530

    Sync: 121. Best Time to Buy and Sell Stock (Easy) - Leet2Git

--- 121_Best_Time_to_Buy_and_Sell_Stock.cpp
+++ 121_Best_Time_to_Buy_and_Sell_Stock.cpp
@@ -1,48 +1,22 @@
 class Solution {
 public:
-    int find(int i, vector<int> &nums, vector<int> &dp) {
-        int n = nums.size();
+    int maxProfit(vector<int>& prices) {
+        int currentCost = prices[0];
+        int minCost = prices[0];
+        int profit = 0;
 
-        if(i == n - 1) return 0;
+        int n = prices.size();
 
-        if(dp[i] != - 1) return dp[i];
+        for(int i = 1; i < n; i++) {
+            currentCost = prices[i];
 
-        int maxProfit = 0;
-
-        for(int j = i + 1; j < n; j++) {
-            int profit = 0;
-            if(nums[j] > nums[i]) {
-                profit = nums[j] - nums[i];
+            if(currentCost < minCost) {
+                minCost = currentCost;
             }
 
-            maxProfit = max(profit, maxProfit);
+            profit = max(profit, currentCost - minCost);
         }
 
-        maxProfit = max(maxProfit, find(i + 1, nums, dp));
-
-        return dp[i] = maxProfit;
-    }
-    int maxProfit(vector<int>& nums) {
-        int n = nums.size();
-
-        vector<int> dp(n, 0); 
-
-
-        for(int i = n - 2; i >= 0; i--) {
-
-            int maxProfit = 0;
-
-            for(int j = i + 1; j < n; j++) {
-                if(nums[j] > nums[i]) {
-                    profit = nums[j] - nums[i];
-                }
-
-                maxProfit = max(Profit, profit);
-            }
-
-            dp[i] = max(maxProfit, dp[i + 1]);
-        }
-
-        return dp[0];
+        return profit;
     }
 };
